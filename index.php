37cc5277f67963f920ab119dbfa7a6f9264037a0
Robbert Feunekes <robbert@serasoft.com>
2014-07-18 15:06:14 +0200

    Check if git has been set up properly

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
-<?php 
-	
+<?php
+
 	require 'blocks/db_connect.php';
-	
+
+
 	$query = "SELECT * FROM `news` ORDER BY `news`.`Date` DESC LIMIT 1";
 
-	$result = mysql_query($query);	
-	
-	require 'blocks/prereqs.php'; 
-	
+	$result = mysql_query($query);
+
+	require 'blocks/prereqs.php';
+
 ?>
 	<!--<div class="container">-->
 	<script src="js/s3Slider.js" type="text/javascript"></script>
-	
+
 		<!--<div class="full nopadding">-->
 			<div id="s3slider">
 				<ul id="s3sliderContent">
@@ -73,15 +74,15 @@
 					</li>
 					<div class="clear s3sliderImage"></div>
 				</ul>-->
-				<?php 
-						
-						// Set the total number of slides to be displayed. 
+				<?php
+
+						// Set the total number of slides to be displayed.
 						// N.B.: May be less than total amount. The number provided is the number of slides shown.
-						$count = 8; 
-						
+						$count = 8;
+
 						// Insert the titles of the images, in the right order
 						$src = array("Announcement1","SimpleStar","EarlyRedGiant","Atmosless","Asteroidbelt","NebulaSystem","SunsetPeak","SpaceVehicle");
-						
+
 						// Define the location for each slide
 						// You may combine   left/right/fullWidth   with   top/bottom/fullHeight
 						$loc[0] = "left bottom";
@@ -92,7 +93,7 @@
 						$loc[5] = "right top";
 						$loc[6] = "left top";
 						$loc[7] = "right top";
-						
+
 						// Fill in the text for the headings
 						$title[0] = "Ship Announcement";
 						$title[1] = "Reach the stars";
@@ -102,8 +103,8 @@
 						$title[5] = "GasFilterPlus&trade;";
 						$title[6] = "Clean Air&trade;";
 						$title[7] = "Riptide Class";
-						
-						// Fill in the body content for the slides. 
+
+						// Fill in the body content for the slides.
 						// N.B.: Use full html formatting! Insert a backslash \ to escape double quotes (so use <a href=\"#\"> instead of <a href="#">)
 						$content[0] = "<p>Soon to be revealed.</p>";
 						$content[1] = "<p>We are there for you.</p>";
@@ -113,8 +114,8 @@
 						$content[5] = "<p>No more limits of where you can go.</p>";
 						$content[6] = "<p>For a safer, healthier future.</p>";
 						$content[7] = "<p>Go fast in style.</p>";
-						
-												
+
+
 						for ($i=0; $i<$count; $i++) {
 							echo "<li class=\"s3sliderImage\"><img src=\"img/front/" . $src[$i] . ".jpg\" width=\"890px\">";
 							echo "<div class=\"" . $loc[$i] . "\">";
@@ -126,7 +127,7 @@
 					?>
 			</div>
 		<!--</div>
-		
+
 	</div>-->
 	<script type="text/javascript">
 		$(document).ready(function() {
