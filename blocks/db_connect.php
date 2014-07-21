eb73eb610c236e93a1a3778839daef9a38c57554
Robbert Feunekes <robbert@serasoft.com>
2014-07-21 13:28:42 +0200

    Local database connection Robbert

--- blocks/db_connect.php
+++ blocks/db_connect.php
@@ -1,7 +1,38 @@
-<? $connect = mysql_connect("localhost", "sphereindu_query", "southpark");
+<?php
 
-	if (!$connect) {
-		die('Could not connect: ' . mysql_error());
-	} 
+$url = explode('.', $_SERVER['HTTP_HOST']);
+define('TLD', end($url));
 
-	mysql_select_db("sphereindu_articles", $connect); ?>
+switch (TLD) {
+
+    case 'robbert':
+        $c = array(
+            'host'  => 'localhost',
+            'user'  => 'root',
+            'pass'  => '',
+            'db'    => 'sphereindu_articles',
+        );
+        break;
+
+    case 'robert':
+        $c = array(
+            'host'  => 'localhost',
+            'user'  => 'sphereindu_query',
+            'pass'  => 'southpark',
+            'db'    => 'sphereindu_articles'
+        );
+        break;
+
+    default:
+        $c = array(
+            'host'  => 'localhost',
+            'user'  => 'sphereindu_query',
+            'pass'  => 'southpark',
+            'db'    => 'sphereindu_articles'
+        );
+
+}
+
+$connect = mysql_connect($c['host'], $c['user'], $c['pass']) or die(mysql_error());
+
+mysql_select_db($c['db'], $connect);
